ad5752d2bfb7c2e91ace3a4e9a5be39171da5bc1
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-01-22 13:53:22 +0530

    Create dict_parsing.py

---
+++ dict_parsing.py
@@ -0,0 +1,15 @@
+#Sort the list 
+l1 = [3, 1, 4, 2, 5]
+l1.sort()
+print(l1)
+
+ 
+#print the squares of all the number in the list 
+l2 = [3, 1, 4, 2, 5]
+l1=[]
+for i in l2:
+  l1.append(i**2)
+print(l1) 
+
+
+#print all the elements in the list 
