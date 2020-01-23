eed197ac77718b58fc88413b55ecdfb6ddf045f3
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-01-23 14:33:30 +0530

    Update Sum of list index.py

--- Sum of list index.py
+++ Sum of list index.py
@@ -5,10 +5,17 @@ for j in range(i):
   l.append(a)
 print(l)
 ind=int(input("Enter Index Value:"))
-s=0
-
+e=0
+o=0
+even=[]
+odd=[]
 for k in range(0,ind+1):
-  s=s+l[k]
-print(s)
-l.append(s)
-print(l)
+  if(l[k]%2==0):
+    e=e+l[k]
+    even.append(e)
+  else:
+    o=o+l[k]
+    odd.append(e)
+
+print("Odd list",o)
+print("Even List",e)
