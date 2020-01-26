553abf0a6a068016634de5964eef3c335a16e285
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-01-26 11:10:23 +0530

    Update NumberGuess.py

--- NumberGuess.py
+++ NumberGuess.py
@@ -2,12 +2,16 @@ from random import randint
 r=randint(0,10)
 #print(r)
 i=0
+count=0
 while(i<5):
   n=int(input("Enter any number within 10:"))
   if (n==r):
     print("Your Guess is correct ")
-    print("!*************ou Won************1")
+    print("!*************You Won************!")
     break
   else:
     print("Try Again")
+    count=count+1
+    if count==5:
+      print("\nAll your Guesses are Wrong...\n:( You Lost The Game :(")
   i=i+1
