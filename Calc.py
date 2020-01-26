7cc0cf00c27a75429d507b3f40c7dc58120dad6a
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-01-26 12:45:10 +0530

    Update Calc.py

--- Calc.py
+++ Calc.py
@@ -1,37 +1,33 @@
-def a():
-  n1=int(input("Enter 1st number:"))
-  n2=int(input("Enter 2nd number:"))
+def a(n1,n2):
+  
   return n1+n2
 
-def s():
-  n1=int(input("Enter 1st number:"))
-  n2=int(input("Enter 2nd number:"))
+def s(n1,n2):
+  
   return n1-n2
 
-def m():
-  n1=int(input("Enter 1st number:"))
-  n2=int(input("Enter 2nd number:"))
+def m(n1,n2):
+  
   return n1*n2
 
-def d():
-  n1=int(input("Enter 1st number:"))
-  n2=int(input("Enter 2nd number:"))
+def d(n1,n2):
+  
   if n2==0:
     print("Division by Zero is not allowed")
   else:
     return n1/n2
 
-def calc(i):
+def calc(i,n1,n2):
 
 
   if i==1:
-    print("Addition Value is",a())
+    print("Addition Value is",a(n1,n2))
   elif i==2:
-    print("Subtraction value is",s())
+    print("Subtraction value is",s(n1,n2))
   elif i==3:
-    print("Multiplication value is",m())
+    print("Multiplication value is",m(n1,n2))
   elif i==4:
-    print("Dicision Value is",d())
+    print("Division Value is",d(n1,n2))
   else:
     print("Kindly Enter a valid choice")
     start()
@@ -45,17 +41,23 @@ def abn():
   print("Thank You!! Seems You Don't need Calculator Genius ")   
   exit 
 
+def numbers():
+  print("NUMBERS\n")
+  print("1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\t\n\t0")
 
 def start():
   c='y'
   while(c=='y'):
+    n1=int(input("Enter 1st number:"))
+    n2=int(input("Enter 2nd number:"))
     print("What would you like to do:")
     print('1:Add 2:Sub 3:Mul 4:Div')
     i=float(input("Enter Your Choice: "))
-    if (i==1 or i==2 or i==3 or i==4):
-      calc(i)
+    if (i<=4):
+      calc(i,n1,n2)
     else:
       abn()
     c=input()
 
+numbers()
 start()
